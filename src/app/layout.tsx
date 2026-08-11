c1de15e11dbdf8aada751cb6dc7814c3e5f3c533
Musa Javed <148046383+musajaved758@users.noreply.github.com>
2026-08-11 15:29:19 +0500

    name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "wacrm",
-    template: "%s — wacrm",
+    default: "Wappo",
+    template: "%s — Wappo",
   },
   description: "Self-hostable CRM template for WhatsApp.",
   robots: {
